e6901868d30059574c00c676393ce9b44ceb7746
smcarik <Steven@carik-pc>
2017-03-27 15:27:01 -0500

    updated phpunit test class with more examples and instructions. READ!!

---
+++ PHPUnitTesting/testphpunit.php
@@ -0,0 +1,33 @@
+<?php
+/* 
+ * start off with class <class name> extends PHPUnit_Framework_TestCase
+ * 
+ * use public function <test name>
+ * note: test name must include the word test, and test must come first
+ * 				<test<test description>>()
+ */
+class testPhpUnit extends PHPUnit_Framework_TestCase {
+	Public function test() {
+		$this->assertTrue(true);
+		$this->assertTrue(5==5);
+	}
+	
+	public function test2(){
+		$this->assertTrue(true);
+	}
+	
+	public function testDoesItEqual(){
+		$this->assertEquals(5,5);
+	}
+	
+	//Notice how only 4 of the 5 tests are run? this one does not run.  comment it out to check for yourself.
+	public function doesTestMatter(){
+		$this->assertTrue(true);
+	}
+	
+	public function testdoesTestMatter(){
+		$this->assertFalse(5==4);
+	}
+}
+
+?>

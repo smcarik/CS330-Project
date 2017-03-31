2e5af447d4ca3e8079d8de5c7a138656eb3c73d7
Matthew Kounniyom <makounniyom@csbsju.edu>
2017-03-31 11:00:05 -0500

    Created a rough outline for US4 Test Criteria.

---
+++ PHPUnitTesting/UserStory4AcceptanceCriteria.php
@@ -0,0 +1,23 @@
+<?php
+// Will edit this accordingly when the US4 gets some body to it. 
+
+//include __DIR__.'/../Controllers/DBController.php';
+
+class testPhpUnit extends PHPUnit_Framework_TestCase 
+{
+	public function testMemberEntersValidUNameAndGetsProj(): void
+	{
+		
+	}
+	
+	public function testFailInviteCurrentMembers(): void
+	{
+		
+	}
+	
+	public function testMustBeMemberToInvite(): void
+	{
+		
+	}
+}
+?>

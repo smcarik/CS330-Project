396f68e9e95b345304ec7c1627b29b05089bd67c
adamnoack <adamnoack@152.65.56.76>
2017-04-01 20:42:03 -0500

    change addIfUnique method to pass/fail based on exception existence

--- Controllers/DBController.php
+++ Controllers/DBController.php
@@ -12,8 +12,8 @@
 	
 		}
 		
-		function addIfUnique($newProjectName, $newProjectDescription){
-			$sql = "SELECT * FROM ProjectInfo";
+		function addIfUnique($newProjectName, $newProjectDescription){ //adds a new project to the project table if the name is unique
+			/* $sql = "SELECT * FROM ProjectInfo";
 			 
 			foreach($db->query($sql) as $row) {
 				if(strcmp($row["Name"],$newProjectName) == 0) { //checks each project name in table to see if it matches input
@@ -24,8 +24,15 @@
 				$sql = "INSERT INTO ProjectInfo (Name, ProjectDesc) VALUES (".$newProjectName.", ".$newProjectDescription.")";
 				$db->exec($sql);
 				return true;
+			} */
+			$sql = "INSERT INTO ProjectInfo (Name, ProjectDesc) VALUES (".$newProjectName.", ".$newProjectDescription.")";
+			try{
+				$db->exec($sql);
 			}
-			return false;
+			catch(Exception $e){
+				return false;
+			}
+			return true;
 		}
 	}
 ?>

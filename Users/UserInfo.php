46dd5278d9013274dc63d609315fc51c52dd9ba3
smcarik <Steven@carik-pc>
2017-04-02 23:50:51 -0500

    created userInfo class to create user objects

---
+++ Users/UserInfo.php
@@ -0,0 +1,34 @@
+<?php
+	class User{
+		var $fname = null;
+		var $lname = null;
+		var $uname = null;
+		public function __construct($fn,$ln,$un){
+			$this->fname = $fn;
+			$this->lname = $ln;
+			$this->uname = $un;
+		}
+		
+		public function setFName($fn){
+			$this->fname = $fn;
+		}
+		
+		public function getFName(){
+			return $this->fname;
+		}
+		public function setLName($fn){
+			$this->lname = $fn;
+		}
+		
+		public function getLName(){
+			return $this->lname;
+		}
+		public function setUName($fn){
+			$this->uname = $fn;
+		}
+		
+		public function getUName(){
+			return $this->uname;
+		}
+	}
+?>

740afd825d3bc1c56ab07774b0acb6a3fe143fa2
erinqueme <erinquemeeastend@gmail.com>
2017-04-03 17:04:14 -0600

    Created the front end UI for invites

---
+++ inviteToProject/invites.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+
+ 	<title>Invite</title>
+ 	
+ 	 <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+	<h1>Please enter a valid username to invite other members.</h1>
+	<form action="invite_action.php" method="POST">
+		<table>
+			<tbody>
+				<tr>
+					<td>UserName:</td>
+					<td><input type="text" name="uid" id="UserName"></td>
+				</tr>
+			</tbody>		
+		</table>		
+		<input type="submit" value="Invite">
+	</form>
+	<p>
+	</p>
+</body>
+</html>

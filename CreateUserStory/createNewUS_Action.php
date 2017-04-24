59e6b7ed004757805b851d786ea6af650bc4133b
erinqueme <erinquemeeastend@gmail.com>
2017-04-24 17:25:58 -0500

    Corrected projectInfo and tried doing the action page for creating a US

--- CreateUserStory/createNewUS_Action.php
+++ CreateUserStory/createNewUS_Action.php
@@ -1,2 +1,19 @@
 <?php
+//session_start();
+include __DIR__.'\..\Controllers\DBController.php';
+include __DIR__.'\..\Projects\projectInfo.php';
+$us = new UserStoryInfo();
+$userStory = $us -> __construct($_POST['id'],$_POST['asa'],$_POST['iwant'],$_POST['inorderto'],$_POST['accept'],$_POST['size'],$_POST['sprint']);
+try 
+{
+	$db = new ContactDB();
+	$db -> addItemToBacklog($userStory);
+	header('/CS330-Project/ProjectHomePage/projectHomePage.php');
+}
+catch (PDOException $e)
+{
+	header('/CS330-Project/ProjectHomePage/projectHomePage.php');
+	echo "Creation failed" . $e->getMessage();
+}
+
 ?>

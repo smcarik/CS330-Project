4a89573946923a23a56bb84ed915e5062a4189c9
erinqueme <erinquemeeastend@gmail.com>
2017-04-25 19:24:04 -0500

    ADDED CSS CODE TO creating US and project Home Page. Added a table to the the project home page (just for a rough view)

--- CreateUserStory/createNewUS.php
+++ CreateUserStory/createNewUS.php
@@ -1,6 +1,7 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
 <html>
 <head>
+ 	 <link rel="stylesheet" href="styles.css">
 <meta http-equiv="Content-Type" content="text/html; charset=Cp1252">
 <title>Create User Story</title>
 </head>
--- ProjectHomePage/projectHomePage.php
+++ ProjectHomePage/projectHomePage.php
@@ -1,15 +1,39 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
 <html>
 <head>
+ 	 <link rel="stylesheet" href="styles.css">
 <meta http-equiv="Content-Type" content="text/html; charset=Cp1252">
+
 <title>Project Home Page</title>
 
 </head>
-    <body>
-    <h1>This is a temporary project page, update later to include all functionality</h1>
+<body>
+	<h1>This is a temporary project page, update later to include all
+		functionality</h1>
     <?php
-    $_SESSION['project']=$_POST['prjName']; //create a session variable for the current project
-    echo $_SESSION['project'];
-	?>
-    </body>
+				$_SESSION ['project'] = $_POST ['prjName']; // create a session variable for the current project
+				echo "Project:". $_SESSION ['project'];
+				
+				?>
+	
+	    <table class="dataentrytable" border="1">
+		<tbody>
+			<tr>
+				<td>Product Backlog</td>
+				<td>Sprint Backlog</td>
+				<td>Tasks To Do</td>
+				<td>Tasks In Progress</td>
+				<td>Tasks Completed</td>
+			</tr>
+			<tr>
+				<td><button onclick="window.location.href='/../CS330-Project/CreateUserStory/CreateNewUS.php'">Create User Stories</button></td>
+				<td> </td>
+				<td><button onclick="window.location.href='/../CS330-Project/inviteToProject/invites.php'">Create a Task</button></td>
+				<td> </td>
+				<td> </td>
+			</tr>
+			
+		</tbody>
+	</table>
+</body>
 </html>

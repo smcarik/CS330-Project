a5a886fdb59fb0c01682161d7a81a145550d339d
Matthew Kounniyom <makounniyom@csbsju.edu>
2017-04-29 13:59:57 -0500

    Stylin'
    Made the login page a bit prettier and less chaotic

--- login/login.php
+++ login/login.php
@@ -9,38 +9,38 @@
 <head>
 
  	<title>Login</title>
- 	
+
  	 <link rel="stylesheet" href="styles.css">
 </head>
 <body>
 	<h1>Welcome to BlazinPretzel's Scrum Board</h1>
-	<?php 
+	<?php
 	if($_SESSION['Error']!= "none"){
 		echo "Error: " . $_SESSION['Error'];
 		$_SESSION['Error'] = "none";
 	}
 	?>
+	<p>
+		ENTER YOUR USER NAME & PASSWORD
+		<br>
+	</p>
 	<form action="login_Action.php" method="POST">
 		<table>
 			<tbody>
 				<tr>
 					<td>UserName:</td>
-					<td><input type="text" name="uid" id="UserName"></td>
+					<td><input type="text" name="uid" id="Username"></td>
 				</tr>
 				<tr>
 					<td>Password:</td>
 					<td><input type="password" name="pw"></td>
 				</tr>
-			</tbody>		
-		</table>		
+			</tbody>
+		</table>
 		<input type="submit" value="Login">
 	</form>
 	<form action="/CS330-Project/register/register.html">
 		<input type="submit" value="Register">
 	</form>
-	<p>
-		ENTER YOUR USER NAME & PASSWORD 
-		<br>
-	</p>
 </body>
 </html>

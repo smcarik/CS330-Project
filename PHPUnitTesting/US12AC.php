6f29149b9c9959c07d0dce1542519513edec09de
adamnoack <adamnoack@152.65.57.15>
2017-05-01 21:46:01 -0500

    uausasudf

--- PHPUnitTesting/US12AC.php
+++ PHPUnitTesting/US12AC.php
@@ -7,15 +7,21 @@ session_start();
 //include __DIR__.'/../Controllers/DBController.php';
 
 class testPhpUnit extends PHPUnit_Framework_TestCase {
-	public function testEditPBLitem()
+	public function testReject()
 	{
 		$db = new ContactDB;
 		$db->setUpDB();
-		$db->inviteToProject("adam", "BourneJason");
-		$b1 = $db->reject("BourneJason", "adam");
-		$db->inviteToProject("adam", "BourneJason");
-		$b2 = $db->accept("BourneJason", "adam");
-		$this->assertTrue($b1&&$b2);
+		$db->inviteToProject("adam", "RitsBits");
+		$b1 = $db->reject("RitsBits", "adam");
+		$this->assertTrue($b1);
+	}
+	public function testAccept()
+	{
+		$db = new ContactDB;
+		$db->setUpDB();
+		$db->inviteToProject("adam", "erin");
+		$b2 = $db->reject("erin", "adam");
+		$this->assertTrue($b2);
 	}
 }
 ?>
